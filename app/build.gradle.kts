1255bbfdbc907ea141ac214eb7e77f392ea4acf4
lrdcxdes <lordgrief176@gmail.com>
2023-08-11 21:54:12 +0300

    Update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 28
         targetSdk = 34
         versionCode = 1
-        versionName = "1.2.4"
+        versionName = "1.2.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
