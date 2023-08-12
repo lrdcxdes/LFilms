1f0aa814165fe029252f62876e9a23b107b9d27e
lrdcxdes <lordgrief176@gmail.com>
2023-08-12 16:22:19 +0300

    [HotFix] getMovie() path

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 28
         targetSdk = 34
         versionCode = 1
-        versionName = "1.2.9-hotfix04"
+        versionName = "1.2.9-hotfix05"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
