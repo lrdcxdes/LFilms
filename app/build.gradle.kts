5e4b8b6fdb8dec0f1daf2f12eac94d2f6e9957a2
lrdcxdes <lordgrief176@gmail.com>
2023-08-12 13:26:17 +0300

    [HotFix] Fatal Exception: javax.net.ssl.SSLHandshakeException

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 28
         targetSdk = 34
         versionCode = 1
-        versionName = "1.2.9"
+        versionName = "1.2.9-hotfix01"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
