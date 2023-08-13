8e56d2f6c7ee7f27fe70c658f32d9d49cbcc1934
lrdcxdes <lordgrief176@gmail.com>
2023-08-14 01:06:00 +0300

    [FIX] Auto-Update client

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     namespace = "dev.lrdcxdes.lfilms"
     compileSdk = 34
 
-    val vName = "1.3.2"
+    val vName = "1.3.3"
 
     defaultConfig {
         applicationId = "dev.lrdcxdes.lfilms"
